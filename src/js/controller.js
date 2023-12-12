dddeabe1d2bba085ce0112c47c2ddfb6b6837ffd
Maxim <118841284+MaximA21@users.noreply.github.com>
2023-12-12 15:17:46 +0100

    listens to hashchange

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,9 +28,12 @@ const renderSpinner = function (parentEl) {
 }
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1)
+
+    if (!id) return
     // 1) loading recipe
     renderSpinner(recipeContainer)
-    //const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886")
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
     const data = await res.json()
     if (!res.ok) throw new Error(`${data.message}(${res.status})`)
     let {recipe} = data.data
@@ -133,5 +136,7 @@ const showRecipe = async function () {
   } catch (e) {
     alert(e)
   }
-}
-showRecipe()
+};
+
+    ['load', 'hashchange'].forEach(evt => window.addEventListener(evt, showRecipe))
+
